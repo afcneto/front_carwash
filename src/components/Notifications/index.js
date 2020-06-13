8e55c4093175ee4b3d0377cfe209a75ed1c87a63
Aderson Neto <afcneto@outlook.com>
2020-06-12 21:33:40 -0300

    notificações estilizadas

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { MdNotifications } from 'react-icons/md';
 
-import { Container, Badge } from './styles';
+import { Container, Badge, Scroll, NotificationList, Notification } from './styles';
 
 export default function Notifications() {
   return (
@@ -10,6 +10,26 @@ export default function Notifications() {
       <Badge hasUnread>
         <MdNotifications color="#f29200" size={20} />
       </Badge>
+
+      <NotificationList>
+        <Scroll>
+          <Notification unread>
+            <p>Novo agendamento</p>
+            <time>há dois dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+          <Notification>
+            <p>Novo agendamento</p>
+            <time>há dois dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+          <Notification>
+            <p>Novo agendamento</p>
+            <time>há dois dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+        </Scroll>
+      </NotificationList>
     </Container>
   );
 }
